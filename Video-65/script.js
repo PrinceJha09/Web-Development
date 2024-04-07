11090f92aef2571f5b15f5030ffa2362c934220c
PrinceJha09 <princekumarjha0901@gmail.com>
2024-04-07 18:07:42 +0530

    added Harry's code for factorial

--- Video-65/script.js
+++ Video-65/script.js
@@ -1,11 +1,23 @@
 let a = parseInt(prompt("Enter a number"));
-let arr = [];
-for (let i = 1; i <=a ; i++){
-    arr.push(i);
+// let arr = [];
+// for (let i = 1; i <=a ; i++){
+//     arr.push(i);
+// }
+
+// let factorial = arr.reduce((prev,curr)=>{
+//     return prev*curr;
+// });
+
+// console.log(factorial);
+
+// Harry's Code:-
+function factorial (number){
+    let arr = Array.from(Array(number+1).keys());
+    let newArr = arr.slice(1);
+    let c = newArr.reduce((a,b) => {
+        return a*b;
+    });
+    console.log(c);
 }
 
-let factorial = arr.reduce((prev,curr)=>{
-    return prev*curr;
-});
-
-console.log(factorial);
+factorial(a);
